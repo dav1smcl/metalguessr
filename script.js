3afa9f54101cc4d5a99ed005056cf35378c13f1b
dav1smcl <51341821+dav1smcl@users.noreply.github.com>
2025-01-30 10:30:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,8 @@ async function preInitialize() {
         var solution = solution.toUpperCase();
         //console.log(solutionIndex);
         //console.log(solution);
-    
+        console.log(band);
+
         var height = 6;
         var width = solution.length;
         //console.log(width);
@@ -71,7 +72,6 @@ function endGame( { solution, solutionIndex, band, height, width, gameOver } ) {
 
 function initialize( { solution, solutionIndex, band, height, width, gameOver } ) {
     //console.log("puzzles/" + solutionIndex + ".png");
-    console.log(solution.id);
     document.getElementById("puzzle").src = "puzzles/" + solutionIndex + ".png";
     document.getElementById("puzzle").style.opacity = 1;
     
